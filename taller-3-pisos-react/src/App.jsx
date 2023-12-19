3837ccb7d245c15ad7aa648d5fea882ee047e49b
LentinulaEdode <carmona.a.esther@gmail.com>
2023-12-19 11:10:45 +0100

    primer commit con archivos iniciales

---
+++ taller-3-pisos-react/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [inputs, setInputs] = useState({
+    surface: '',
+    bedrooms: '',
+    restrooms: ''
+  });
+
+  const [prediction, setPrediction] = useState(null);
+
+  const handleChange = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(inputs)
+    });
+
+    const data = await response.json();
+
+    setPrediction(data.prediction);
+  };
+
+  return (
+    <div className="App">
+      <h1>Housing Price Prediction</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="surface">Surface:</label>
+        <input type="text" id="surface" name="surface" value={inputs.surface} onChange={handleChange} />
+
+        <label htmlFor="bedrooms">Bedrooms:</label>
+        <input type="text" id="bedrooms" name="bedrooms" value={inputs.bedrooms} onChange={handleChange} />
+
+        <label htmlFor="restrooms">Restrooms:</label>
+        <input type="text" id="restrooms" name="restrooms" value={inputs.restrooms} onChange={handleChange} />
+
+        <button type="submit">Predict</button>
+      </form>
+
+      {prediction && (
+        <p>Predicted Price: {prediction}</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
